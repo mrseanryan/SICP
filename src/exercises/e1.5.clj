805a2f73cf1852627c6a1805a9dec1e72a7028a5
mrseanryan <str_ie@yahoo.co.uk>
2019-03-09 20:36:02 +0000

    Add ex1.5

---
+++ src/exercises/e1.5.clj
@@ -0,0 +1,10 @@
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+    0
+    y))
+
+; note this will not terminate, due to applicative ('apply') interpretation:
+; (p) is evaluated BEFORE the body of test
+(test 0 (p))
