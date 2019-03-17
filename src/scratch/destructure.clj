5910f8be61c4f2dabacc9984b0115929cbed8212
sean <str_ie@yahoo.co.uk>
2019-03-17 10:15:31 +0100

    Add scratch dir with attempt at destructuring

---
+++ src/scratch/destructure.clj
@@ -0,0 +1,7 @@
+; ref: http://www.phyast.pitt.edu/~micheles/scheme/scheme15.html
+
+; TODO fix - needs a library? OR needs 'chicken' instead of mit-scheme
+
+(match-define (head . tail) '(1 2 3))
+
+(list head tail)
