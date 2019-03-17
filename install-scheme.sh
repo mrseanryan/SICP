44b4a6221d855176f028d748ee48af9c51327df5
sean <str_ie@yahoo.co.uk>
2019-03-17 10:15:06 +0100

    Add rlwrap to help with REPL

--- install-scheme.sh
+++ install-scheme.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get install mit-scheme
+
+# rlwrap (readline wrap) makes Scheme REPL more usable
+sudo apt-get install rlwrap
---
+++ repl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# REPL (Read-Eval-Print-Loop)
+
+# rlwrap (readline wrap) makes Scheme REPL more usable
+# adds cli history and bracket matching!
+rlwrap mit-scheme
