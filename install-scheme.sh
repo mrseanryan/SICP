eb93f2c6c78630badbfd85f3a841d19d557c0c30
Sean Ryan <sean.ryan@mendix.com>
2019-03-27 11:57:26 +0100

    Add SICP support to Racket

--- install-scheme.sh
+++ install-scheme.sh
@@ -4,6 +4,9 @@ sudo apt-get install mit-scheme
 # rlwrap (readline wrap) makes Scheme REPL more usable
 sudo apt-get install rlwrap
 
-# racket is possibly the most complete implementation of Scheme,
+# Racket is possibly the most complete implementation of Scheme,
 # and has its own IDE (DrRacket)
 sudo apt-get install racket
+
+# Add SICP support to Racket
+raco pkg install sicp
