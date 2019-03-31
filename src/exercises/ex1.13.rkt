484d9da9f762ec16eb9e318058f883e4584b89c3
sean <str_ie@yahoo.co.uk>
2019-03-31 19:38:31 +0200

    Finish ex1.13

--- src/exercises/ex1.13.rkt
+++ src/exercises/ex1.13.rkt
@@ -3,13 +3,13 @@
 (require racket/trace)
 
 (display "_____________________________\n")
-(display "Prove Fib(n) is the closest integer to theta^n//sqrt(5) \n");
+(display "Prove Fib(n) is the closest integer to phi^n//sqrt(5) \n");
 
 ; Induction: f(0), f(n), f(n+1) -> proven for all n natural numbers.
 
 
-
-
+; __________________________
+; FIBONACCI
 (define (fib n)
   (fib-iter 1 0 n)
   )
@@ -21,16 +21,66 @@
   )
 
 
-(define (test-fun func n)
-  (display (format "fib(~a) = ~a\n" n (func n)))
-  )
-  
+; __________________________
+; phi function
 
+(define phi
+  (/ (+ 1 (sqrt 5) )
+     2
+     )
+  )
+
+(define (pow x n)
+  (define (pow-inner n count)
+    (if (= count n) 1
+        (* x (pow-inner n (+ count 1)))
+        )
+    )
+  (pow-inner n 0)
+  )
+
+(define (phi-fun n)
+  (/ (pow phi n)
+     (sqrt 5)
+     )
+  )
+
+(define gamma
+  (/ (- 1 (sqrt 5))
+     2)
+  )
+
+(define (fib-via-phi n)
+  (/ (- (pow phi n) (pow gamma n) )
+     (sqrt 5))
+  )
+
+;___________________________
+; test
+
+
+(define (test-fun name func n)
+  (display (format "~a(~a) = ~a\n" name n (func n)))
+  )
+
+(define (fib-test n)
+  (test-fun "fib" fib n)
+  (test-fun "fib-via-phi" fib-via-phi n)
+  )
+
+; __________________________
 ;(trace fun)
 
-(test-fun fib 1)
-(test-fun fib 2)
-(test-fun fib 3)
-(test-fun fib 4)
-(test-fun fib 5)
-(test-fun fib 6)
+; INDUCTION
+
+; n = 0
+(fib-test 0)
+
+; n = 1
+(fib-test 1)
+; n = 2
+(fib-test 2)
+(fib-test 3)
+(fib-test 4)
+(fib-test 5)
+(fib-test 6)
