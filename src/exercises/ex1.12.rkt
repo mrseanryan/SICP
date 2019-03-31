3ccb55123b4af214e7d09efd4d5f0a2fcf8a8168
sean <str_ie@yahoo.co.uk>
2019-03-31 19:09:44 +0200

    Show the binomial expansion with Pascals triangle ex1.12

--- src/exercises/ex1.12.rkt
+++ src/exercises/ex1.12.rkt
@@ -2,10 +2,8 @@
 (require sicp)
 (require racket/trace)
 
-; Pascal's triangle
-
 (display "_____________________________\n")
-(display "first attempt \n");
+(display "Pascal's triangle -> binomial expansion \n");
 
 (define (fun-cell row column)
   (cond ((= row 1) 1)
@@ -18,13 +16,47 @@
                 ))
   )
 
-(define (fun-row row cell)
-  (display (format "~s " (fun-cell row cell)))
-
-  (if (< cell row) 
-      (fun-row row (+ cell 1))
-      )    
+; BEGIN FORMATTING
+(define (pow-str num exp)
+  (cond ((= exp 0) 1)
+        ( (= exp 1) num )
+        (else (format "~a^~a" num exp))
+        )
   )
+
+(define (pow num exp)
+  (pow-str num (- exp 1))
+  )
+
+; get the binomial expansion
+(define (get-exponentials row cell)
+  (cond ((= cell 1)  (pow "x" row) )
+        ((= cell row) (pow "y" row) )
+        (else
+         (format "~a.~a" (pow "x" (inc (- row cell))) (pow "y" cell) )
+         ) 
+        )
+  )
+
+(define (get-plus row cell)
+  (if (< cell row) "+ " "")
+  )
+
+(define (no-ones n)
+  (if (= n 1) "" n )
+  )
+
+(define (format-row-cell row cell)
+  (format "~a~a ~a"  (no-ones (fun-cell row cell)) (get-exponentials row cell) (get-plus row cell)  )
+  )
+
+(define (fun-row row cell)  
+  (if (< cell row)
+      (format "~a~a" (format-row-cell row cell) (fun-row row (+ cell 1)))
+      (format-row-cell row cell)
+      )
+  )
+; END FORMATTING
 
 (define (fun row)
   (fun-row row 1)
@@ -32,10 +64,10 @@
 
 
 (define (test-fun func n)
-  (display (format "fun ~s = ~s\n" n (func n)))
+  (display (format "~a = ~a\n" (pow "(x + y)" n) (func n)))
   )
 
-(trace fun)
+;(trace fun)
 
 (test-fun fun 1)
 (test-fun fun 2)
@@ -43,3 +75,7 @@
 (test-fun fun 4)
 (test-fun fun 5)
 (test-fun fun 6)
+(test-fun fun 7)
+(test-fun fun 8)
+(test-fun fun 9)
+(test-fun fun 10)
