da1e554e5d58330eba05aedb06624ec032e15b39
sean <str_ie@yahoo.co.uk>
2019-04-07 12:01:42 +0200

    Profile ex1.16 - original seems faster

--- src/exercises/sean/ex1.16.rkt
+++ src/exercises/sean/ex1.16.rkt
@@ -33,8 +33,26 @@
 
 (provide my-expt)
 
+(define (repeat-fun fun count result)
+  (cond ((= count 0) 0)
+        (else 
+         (repeat-fun fun (- count 1) (fun) )
+         )
+        )
+  )
 
-;(trace sine)
-;(trace p)
-;
-;(sine 12.15)
+(define (time-repeated-fun fun)
+  (time (repeat-fun fun 100000 0))
+  )
+
+(define (test-expt)
+  (fast-expt 10 1000)
+  )
+
+(define (test-expt-2)
+  (fast-expt-2 10 1000 1)
+  )
+
+; original fun performs slightly faster!
+(time-repeated-fun test-expt)
+(time-repeated-fun test-expt-2)
