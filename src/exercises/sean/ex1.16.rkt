9df8f202a2aefa8bf768698d4eca36c9b43294fe
sean <str_ie@yahoo.co.uk>
2019-04-07 11:49:49 +0200

    Solution for ex1.16

--- src/exercises/sean/ex1.16.rkt
+++ src/exercises/sean/ex1.16.rkt
@@ -13,7 +13,25 @@
         )
   )
 
-(provide fast-expt)
+ 
+(define (fast-expt-2 b n a)
+  (cond (( = n 0) a)
+        ( (even? n)
+          ;(square (fast-expt-2 b (/ n 2)  )  )
+          ; = B^2 ^ n/2
+          (fast-expt-2  (square b) (/ n 2) a)
+          )
+        ; (else (* b (fast-expt b (- n 1) )  ) )
+        (else (fast-expt-2 b (- n 1)  (* a b) ) )
+        )
+  )
+
+
+(define (my-expt b n)
+  (fast-expt-2 b n 1)
+  )
+
+(provide my-expt)
 
 
 ;(trace sine)
--- src/exercises/sean/ex1.16.test.rkt
+++ src/exercises/sean/ex1.16.test.rkt
@@ -5,17 +5,17 @@
 (require "ex1.16.rkt")
 
 (chk
- (fast-expt 1 0) 1
- (fast-expt 2 0) 1
- (fast-expt 10 0) 1
+ (my-expt 1 0) 1
+ (my-expt 2 0) 1
+ (my-expt 10 0) 1
 
- (fast-expt 1 2) 1
- (fast-expt 2 2) 4
- (fast-expt 10 2) 100
+ (my-expt 1 2) 1
+ (my-expt 2 2) 4
+ (my-expt 10 2) 100
 
- (fast-expt 1 3) 1
- (fast-expt 2 3) 8
- (fast-expt 10 3) 1000
+ (my-expt 1 3) 1
+ (my-expt 2 3) 8
+ (my-expt 10 3) 1000
 
  )
 
