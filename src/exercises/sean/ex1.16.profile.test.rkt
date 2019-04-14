274a1dc46929fb00ed09bb26c0c99fa6de4407e3
sean <str_ie@yahoo.co.uk>
2019-04-14 17:20:26 +0200

    Adjust ex1.16 to test with bigger exponents

--- src/exercises/sean/ex1.16.profile.test.rkt
+++ src/exercises/sean/ex1.16.profile.test.rkt
@@ -8,21 +8,25 @@
 ; profiling tests (time taken)
 
 (define (test-expt-basic)
-  (basic-iter-expt 10 1000)
+  (basic-iter-expt 10 10000)
+  )
+
+(define (test-expt-10000)
+  (fast-expt 10 10000)
   )
 
 (define (test-expt)
-  (fast-expt 10 1000)
+  (fast-expt 10000 20000)
   )
 
 (define (test-expt-2)
-  (fast-expt-2 10 1000)
+  (fast-expt-2 10000 20000)
   )
 
 ; basic iter is really slow
 (time-repeated-fun test-expt-basic 50 "basic iter")
-(time-repeated-fun test-expt 50 "successive squaring iter")
+(time-repeated-fun test-expt-10000 50 "successive squaring iter")
 
-; This one performs slightly faster!
-(time-repeated-fun test-expt 100000 "successive squaring iter")
-(time-repeated-fun test-expt-2 100000 "successive squaring iter - invariant quantity")
+; even with bigger exponents, the successive-squaring performs the best
+(time-repeated-fun test-expt 10000 "successive squaring iter")
+(time-repeated-fun test-expt-2 10000 "successive squaring iter - invariant quantity")
