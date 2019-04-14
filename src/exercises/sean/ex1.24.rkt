7625a5685fba8e6f39ce9c70cea7ef1fb66ed1ce
sean <str_ie@yahoo.co.uk>
2019-04-14 18:51:07 +0200

    Optimize output of ex1.24 and note range for which perf is logarithmic (on tablet)

--- src/exercises/sean/ex1.24.rkt
+++ src/exercises/sean/ex1.24.rkt
@@ -6,20 +6,18 @@
 (require "utils-profiling-time.rkt")
                             
 (define (prime-test n)
-  (display n)
   (if (fast-prime? n)
-      (report-is-prime)
+      (report-is-prime n)
       (report-is-not-prime)
       )
   )
 
-(define (report-is-prime)
-  (display "* ")
+(define (report-is-prime n)
+  (display (format "~a* " n))
   #t
   )
 
 (define (report-is-not-prime)
-  (display " ")
   #f
   )
 
@@ -47,8 +45,8 @@
 ; profile code - repeating to avoid spurious results (e.g. due to CPU or disk contention)
 (time-repeated-fun (lambda () (fast-prime? 97)) 1000 "fast-prime? - 97")
 
-(define (profile-primes n)
-  (time-repeated-fun (lambda () (search-for-primes n)) 10 (format "search-for-primes - ~a" n))
+(define (profile-primes n [times 10])
+  (time-repeated-fun (lambda () (search-for-primes n)) times (format "search-for-primes - ~a" n))
   )
 
 (trace search-for-primes)
@@ -59,18 +57,20 @@
 (profile-primes 100000)
 (profile-primes 1000000)
 
+(display "_________________________\n")
 (display "bigger numbers\n")
-(profile-primes 1000000000) ; 1e9
-;(profile-primes 10000000000) ; 1e10 - too big for random
-;(profile-primes 1e11) ;1e11 - too big for random
+(profile-primes 1000000000 3) ; 1e9 = 50ms
+(profile-primes 1000000000000000000 3) ; 1e18 = 100ms
+(profile-primes 1000000000000000000000000000000000000 3) ; 1e36 = 200ms
+(profile-primes 1000000000000000000000000000000000000000000000000000000000000000000000000 3) ; 1e72 = 1400ms
 
-(display "note: 100000 x input increase -> 2 x duration\n")
 (display "logarithmic growth: 2 x digts -> 2 x duration\n")
+(display " - on Acer tablet roughly holds true for 1e9..1e36\n")
+(display " but 1e72 is 1e36 x 7: overhead of number size, and display?\n")
+(display " for smaller numbers, factors other than the number size seem to hold (so, for smaller numbers, the number size does not matter)\n")
 ; ref: http://community.schemewiki.org/?sicp-ex-1.24
 (display "  quote: operations on large integers (above the normal 32/64-bit limit) are not constant in time, but functions of the number's size*, as mentioned by torinmr.\n" )
-;100000000000 612 - 10e11
-;10000 357 - 10e4
 
 ; dump code
 search-for-primes-count
-prime?
+fast-prime?
