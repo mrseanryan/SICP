e664315d0444005002e25d6be598094eb7c59ea9
Sean Ryan <sean.ryan@mendix.com>
2019-04-23 10:12:25 +0200

    Add notes on order of growth for ex1.14 and 1.15

--- src/exercises/sean/ex1.14.rkt
+++ src/exercises/sean/ex1.14.rkt
@@ -40,3 +40,21 @@
 (provide count-change cc)
 
 ; TODO optimize - p.41 -> via memoization - ex 3.27 p. 272
+
+; Orders of Growth:
+; ref: http://community.schemewiki.org/?sicp-ex-1.14
+;
+; Space required (maximum height of the call tree) grows linearly with the amount, 
+; as it is determined by the number of times the smallest denomination divides into the amount. i.e. O(a)
+; Time required (number of operations) grows in relation to O(a^n). 
+; i.e. O(a * a * a * ...) since the 2nd branch is O(a), and the first branch is called O(n) times.
+;
+; count-change space order of growth:
+; O(n) because max depth is n
+;
+; count-change time order of growth:
+; cc(n, 1) = O(n)
+; cc(n, 2) = cc(n, 1) + cc(n-5, 2)
+; each 2. step is O(n) and there are roughly n/5 such steps
+; so we have O(n^2)
+; by analogue we get O(n^k) (k currencies) for cc(n, k)
--- src/exercises/sean/ex1.15.rkt
+++ src/exercises/sean/ex1.15.rkt
@@ -24,3 +24,12 @@
 ;(trace p)
 ;
 ;(sine 12.15)
+
+; Orders of Growth:
+; ref: http://community.schemewiki.org/?sicp-ex-1.15
+;
+; from the output, procedure p is applied five times
+;
+; The angle a is divided by 3 each time the procedure p is applied. Expressing this differently, we can say that p is applied 
+; once for each complete power of 3 contained within the angle a. 
+; -> base 3 logarithm -> O(log n) order of growth.
